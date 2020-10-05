1622427f610c8d52aff548d81acf9e793e3a6ad4
Erik Madsen <beatmadsen@gmail.com>
2020-10-05 23:28:29 +0200

    EM: different UDP sockets for in, out

--- lib/gossip/integration/udp/node.rb
+++ lib/gossip/integration/udp/node.rb
@@ -15,8 +15,9 @@ module Gossip
         protected
 
         def before_start
-          @s = UDPSocket.new
-          @s.bind(nil, @port)
+          @in_s = UDPSocket.new
+          @out_s = UDPSocket.new
+          @in_s.bind(nil, @port)
           logger.info "node listening on UDP port #{@port}"
           Thread.new { receive }.tap { |t| t.abort_on_exception = true }
           Thread.new { send }.tap { |t| t.abort_on_exception = true }
@@ -31,7 +32,7 @@ module Gossip
               wire_message = @serializer.serialize(message)
               host = @directory.host(message.to)
               logger.debug "about to send message to #{host}"
-              @s.send(wire_message, 0, host, @port)
+              @out_s.send(wire_message, 0, host, @port)
             rescue StandardError => e
               logger.debug("Error while sending: #{e}")
             end
@@ -43,7 +44,7 @@ module Gossip
           loop do
             begin
               logger.debug 'about to recieve message'
-              text, sender = @s.recvfrom(10_000)
+              text, sender = @in_s.recvfrom(10_000)
 
               message = @deserializer.deserialize(sender[3], text)
               logger.debug "received #{message}"
